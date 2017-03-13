3c621ee1ac65e3346cb56af67ed413d932e48f65
Jonathan Finch <thefinchmeister@gmail.com>
2017-03-13 23:43:41 +0000

    Testing caching

--- service-worker.js
+++ service-worker.js
@@ -15,13 +15,13 @@
 var dataCacheName = 'trainData-v1';
 var cacheName = 'trainPWA-final-1';
 var filesToCache = [
-  '../',
-  '../index.html',
-  '../scripts/app.js',
-  '../styles/inline.css',
-  '../images/trainBackground',
-  '../images/ic_add_white_24px.svg',
-  '../images/ic_refresh_white_24px.svg'
+  '',
+  'index.html',
+  'scripts/app.js',
+  'styles/inline.css',
+  'images/trainBackground',
+  'images/ic_add_white_24px.svg',
+  'images/ic_refresh_white_24px.svg'
 ];
 
 self.addEventListener('install', function(e) {
